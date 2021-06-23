65f90868aadde729bc224fbf2db66be624859e9f
Christian Beddows <christian.beddows@york.ac.uk>
2021-06-23 16:00:00 +0100

    Flesh out test for lighthouse reporting process

--- src/test/lighthouse.test.js
+++ src/test/lighthouse.test.js
@@ -1,7 +1,38 @@
+const fs = require("fs");
 const { lighthouseReport } = require("../main/lighthouse");
 
-describe("lighthouse", () => {
-    it("test", () => {
-        expect(lighthouseReport).not.toThrowError();
-    });
+process.env["INPUT_ITERATIONS"] = 2;
+process.env["INPUT_URLS"] = "https://www.google.com\n" + "https://www.bbc.co.uk\n" + "https://www.york.ac.uk\n";
+
+describe("lighthouseReport", () => {
+  beforeEach(() => {
+    clearDownDir("./reports");
+    clearDownDir("./results");
+  });
+
+  it("Audits the list of input URLS and generates reports", async () => {
+    const [scores, isSuccess] = await lighthouseReport();
+    const numberOfReports = fs.readdirSync('./reports').length;
+    const numberOfResultFiles = fs.readdirSync('./results').length;
+
+    expect(isSuccess).not.toBeNull();
+    expect(scores).toHaveLength(3);
+    expect(scores[1].url).toBe("https://www.bbc.co.uk");
+    expect(scores[1].desktop).not.toBeNull();
+    expect(scores[1].mobile).not.toBeNull();
+    expect(numberOfReports).toEqual(12);
+    expect(numberOfResultFiles).toEqual(1);
+  }, 300000);
 });
+
+const clearDownDir = (path) => {
+  if (fs.existsSync(path)) {
+    fs.rmdir(path, { recursive: true },
+      (error) => {
+        if (error) {
+          console.error(error);
+        }
+      });
+  }
+};
+
